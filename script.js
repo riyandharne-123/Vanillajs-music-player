a1063e3c9cae1eada12c66c55e45363df8520b62
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-08-28 20:43:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+myStorage = window.localStorage;
+
+function save(){
+  var audio=document.querySelector('#audio');
+  // var x = document.getElementById("audio").duration;
+  var x = document.getElementById("audio").currentTime;
+  console.log(x);
+  myStorage.setItem("resumeTime", x);
+  var time =myStorage.getItem('resumeTime');
+    document.getElementById('resumeTime').innerText=time+'s';
+
+}
+
+  function resume(){
+    document.getElementById("audio").currentTime=myStorage.getItem('resumeTime');
+  }
